f21b47f5a5347c76a71bc31411e5ffc163043f59
Antje van Oosten <antjevanoosten@gmail.com>
2022-07-12 17:04:27 +0200

    clean up comments

--- robocoppi.py
+++ robocoppi.py
@@ -37,35 +37,36 @@ for intent in data['intents']:
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
 
-
+#create a list of all the unique stemmed words (turn all the words into lower case, use a set for getting the unique values and get the stem of all the words). Questionmarks are not words. //TODO add esclamation marks/... to these exceptions.
+#the order of the words in the sentence is lost, but the lists 'words' and 'labels' are sorted. RoboCoppi only knows the presence of words in our models vocabulary, not their order in the sentence.
 
 words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
 
-#create a list of all the unique stemmed words (turn all the words into lower case, use a set for getting the unique values and get the stem of all the words). Questionmarks are not words. avo: add esclamation marks/... to these exceptions? Yes.
-#coppi: sorting the list why? To match with the lables list (as this is sorted too)? Wasn't a bag of words unsorted?
-#avo: the order of the words in the sentence is lost, but the lists 'words' and 'labels' are sorted. RoboCoppi only knows the presence of words in our models vocabulary, not their order in the sentence.
-#techtim: "I just sort these because it looks a bit nicer" (avo: so I guess it is not necessary and same thing for 'labels').
+#create empty lists for formatting our input and output.
 
 training = []
 output = []
 
-#create empty lists for formatting our input and output.
+#every class (tag in this case) in our output is 0 to start with
+#the output lists are the length of how many tags (classes) there are. Looping through those lists the value is set to 0 for each tag/class.
 
 out_empty = [0 for _ in range(len(labels))]
 
-#techtim: every class (tag in this case) in our output is 0 to start with
-#avo: the output lists are the length of how many tags (classes) there are. Looping through those lists the value is set to 0 for each tag/class.
+#we could have stemmed the words when preprocessing the data (block starting at line 26, the preprocessing block).
 
 for x, doc in enumerate(docs_x):
     bag = []
 
     wrds = [stemmer.stem(w.lower()) for w in doc]
-
-#we could have stemmed the words when preprocessing the data (block starting at line 26, the preprocessing block).
-
+    
+#represent each sentence with the length of the amount of words in our models vocabulary.
+#each position in the list will represent a word from our vocabulary.
+#if the position in the list is a 1, this means the word exists in our sentence. 0 for not present.
+#creates a new binary feature for each possible category and assigns a value of 1 to the feature of each sample that corresponds to its original category. This is called One Hot Encoding, where hot stands for True.
+   
     for w in words:
         if w in wrds:
             bag.append(1)
@@ -77,16 +78,13 @@ for x, doc in enumerate(docs_x):
 
     training.append(bag)
     output.append(output_row)
-
-#represent each sentence with the length of the amount of words in our models vocabulary.
-#each position in the list will represent a word from our vocabulary.
-#if the position in the list is a 1, this means the word exists in our sentence. 0 for not present.
-#creates a new binary feature for each possible category and assigns a value of 1 to the feature of each sample that corresponds to its original category. This is called One Hot Encoding, where hot stands for True. The truth is hot indeed.
+    
+#convert our training data and output to numpy arrays.
 
 training = numpy.array(training)
 output = numpy.array(output)
 
-#convert our training data and output to numpy arrays.
+#define the architecture of our model. This model is a feed-forward neural network with two hidden layers. You can change the number of neurons (8 per layer in this example) in the layers to see what happens.
 
 tensorflow.compat.v1.reset_default_graph()
 
@@ -98,14 +96,12 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-#define the architecture of our model. This model is a feed-forward neural network with two hidden layers. You can change the number of neurons (8 per layer in this example) in the layers to see what happens.
-
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
-
 #training and saving our model.
 #fit our data to the model. The number of epochs we set is the amount of times that the model will see the same information while training. You can change its value to see what happens.
 #save our data to the file model.tflearn for use in other scripts.
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
 
 # TODO - review following code
 
@@ -133,9 +129,10 @@ def chat():
         results = model.predict([bag_of_words(inp, words)])[0]
         results_index = numpy.argmax(results)
         tag = labels[results_index]
+        
+#This sets the treshold to 70% correctness
 
         if results[results_index] > 0.7:
-#This sets the treshold to 70% correctness
             for tg in data["intents"]:
                 if tg['tag'] == tag:
                     responses = tg['responses']
@@ -147,7 +144,6 @@ def chat():
 chat()
 
 # Get some input from the user
-# It doesn't seem to get beyond this step (script works up to line 101). I went back to the 101 tutorials:) and changed to vs code. After getting really confused by installation of packages and the different Python versions on Mac and trying the following: Installed 'Random Everything' (in Sublime Text) and 'Anaconda', as PIP connecting to 1 Python only situation (1 error message to solve left), no change.
 # Convert it to a bag of words
 # Get a prediction from the model
 # Find the most probable class
