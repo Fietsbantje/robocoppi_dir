24cf2de45e8b06a56f48775c0b6ce5e85ed6283c
coppi <fietsbantje@freedom.nl>
2022-08-15 16:03:31 +0200

    Improve variable names and comments

--- robocoppi.py
+++ robocoppi.py
@@ -1,90 +1,91 @@
-#import natural language tool kit and the stemmer
+# Import the natural language tool kit and the stemmer.
 
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-# FIXME - move comments so they precede the relevant code
-
-# import the following packages //FIXME - resolve issues with these packages
+# Import the following packages.
 
 import numpy
 import tflearn
 import tensorflow
 import random
 
-# import json file with the intents
+# Import json file with the training data (the intents).
 
 import json
 
 with open('intents.json') as file:
     data = json.load(file)
     
-#preprocessing of the data : tokenize, create lists for words and add tags to the intents
+# Preprocessing of the data. Create lists for: the words in every training_phrase, for the intent_names, for the tokenized_training_phrases and for the (matching) intent_name_tags.
 
 words = []
-labels = []
-docs_x = []
-docs_y = []
+intent_names = []
+tokenized_training_phrases = []
+intent_name_tags = []
+
+# Looping through the intents.json file: tokenize the training_phrases and add them to the tokenized_training_phrases list. And add intent_name for every training_phrase to the intent_name_tags list.
 
 for intent in data['intents']:
-    for pattern in intent['patterns']:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
+    for training_phrase in intent['training_phrases']:
+        preprocessed_words = nltk.word_tokenize(training_phrase)
+        words.extend(preprocessed_words)
+        tokenized_training_phrases.append(preprocessed_words)
+        intent_name_tags.append(intent['intent_name_tag'])
         
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])
+    if intent['intent_name_tag'] not in intent_names:
+        intent_names.append(intent['intent_name_tag'])
 
-#create a list of all the unique stemmed words (turn all the words into lower case, use a set for getting the unique values and get the stem of all the words). Questionmarks are not words. //TODO add esclamation marks/... to these exceptions.
-#the order of the words in the sentence is lost, but the lists 'words' and 'labels' are sorted. RoboCoppi only knows the presence of words in our models vocabulary, not their order in the sentence.
+# Create a list of all the unique stemmed words (turn all the words into lower case, use a set for getting the unique values and get the stem of all the words). Questionmarks are not words. //TODO add esclamation marks/... to these exceptions.
+# The order of the words in the sentence is lost, but the lists 'words' and 'intent_names' are sorted. Robocoppi only knows the presence of words in his model's vocabulary, not their order in the sentence.
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = [stemmer.stem(word.lower()) for word in words if word != "?"]
 words = sorted(list(set(words)))
 
-labels = sorted(labels)
+intent_names = sorted(intent_names)
 
-#create empty lists for formatting our input and output.
+# Create empty lists for formatting our input and output.
 
 training = []
 output = []
 
-#every class (tag in this case) in our output is 0 to start with
-#the output lists are the length of how many tags (classes) there are. Looping through those lists the value is set to 0 for each tag/class.
+# The output lists are the length of how many intent names there are. Looping through those lists the value is set to 0 for each intent_name.
+# Hence every intent_name in our output has a value of 0 to start with.
+# Represent each sentence with the length of the amount of words in our models vocabulary.
+# Each position in the list will represent a word from our vocabulary.
 
-out_empty = [0 for _ in range(len(labels))]
+out_empty = [0 for _ in range(len(intent_names))]
 
-#we could have stemmed the words when preprocessing the data (block starting at line 26, the preprocessing block).
+# Create a bag of words for training the model.
 
-for x, doc in enumerate(docs_x):
+for i, token in enumerate(tokenized_training_phrases):
     bag = []
 
-    wrds = [stemmer.stem(w.lower()) for w in doc]
+    preprocessed_words = [stemmer.stem(word.lower()) for word in token]
     
-#represent each sentence with the length of the amount of words in our models vocabulary.
-#each position in the list will represent a word from our vocabulary.
-#if the position in the list is a 1, this means the word exists in our sentence. 0 for not present.
-#creates a new binary feature for each possible category and assigns a value of 1 to the feature of each sample that corresponds to its original category. This is called One Hot Encoding, where hot stands for True.
-   
-    for w in words:
-        if w in wrds:
+# If the position in the list is a 1, this means the word exists in our sentence. 0 for not present.
+
+    for word in words:
+        if word in preprocessed_words:
             bag.append(1)
         else:
             bag.append(0)
 
+# Create a new binary feature for each possible category and assign a value of 1 to the feature of each sample that corresponds to its original category. This is called "one hot encoding", where "hot" stands for True.
+   
     output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+    output_row[intent_names.index(intent_name_tags[i])] = 1
 
     training.append(bag)
     output.append(output_row)
     
-#convert our training data and output to numpy arrays.
+# Convert our training data and output to numpy arrays.
 
 training = numpy.array(training)
 output = numpy.array(output)
 
-#define the architecture of our model. This model is a feed-forward neural network with two hidden layers. You can change the number of neurons (8 per layer in this example) in the layers to see what happens.
+# Define the architecture of our model. This model is a feed-forward neural network with two hidden layers. You can change the number of neurons (8 per layer in this example) in the layers to see what happens.
 
 tensorflow.compat.v1.reset_default_graph()
 
@@ -96,55 +97,56 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-#training and saving our model.
-#fit our data to the model. The number of epochs we set is the amount of times that the model will see the same information while training. You can change its value to see what happens.
-#save our data to the file model.tflearn for use in other scripts.
+# Training and saving our model.
+# Fit our data to the model. The number of epochs we set is the amount of times that the model will see the same information while training. You can change its value to see what happens.
+# Save our data to the file model.tflearn for use in other scripts.
 
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 model.save("model.tflearn")
 
-# TODO - review following code
+# Convert input into a bag of words.
 
-def bag_of_words(s, words):
+def bag_of_words(input_message, words):
     bag = [0 for _ in range(len(words))]
 
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    tokenized_input_message = nltk.word_tokenize(input_message)
+    stemmed_input_message = [stemmer.stem(word.lower()) for word in tokenized_input_message]
 
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
+    for input_word in stemmed_input_message:
+        for i, word in enumerate(words):
+            if word == input_word:
                 bag[i] = 1
             
     return numpy.array(bag)
 
+# Get input from the user.
+# If input is 'basta' stop the program.
 
 def chat():
-    print("Start talking with the bot (type quit to stop)!")
+    print("Talk to me whenever you are ready! Type basta to stop")
     while True:
-        inp = input("You: ")
-        if inp.lower() == "quit":
+        input_message = input("You: ")
+        if input_message.lower() == "basta":
             break
 
-        results = model.predict([bag_of_words(inp, words)])[0]
+# Get a prediction from the model.
+
+        results = model.predict([bag_of_words(input_message, words)])[0]
         results_index = numpy.argmax(results)
-        tag = labels[results_index]
+        prediction = intent_names[results_index]
         
-#This sets the treshold to 70% correctness
+# Set the treshold to 70% correctness.
+# Find the most probable intent.
+# Pick a response from that intent randomly.
+
 
         if results[results_index] > 0.7:
-            for tg in data["intents"]:
-                if tg['tag'] == tag:
-                    responses = tg['responses']
+            for tag in data["intents"]:
+                if tag['intent_name_tag'] == prediction:
+                    responses = tag['responses']
 
             print(random.choice(responses))
         else:
             print("I totally hear you. I just can't really wrap my whiskers around it yet.")
 
 chat()
-
-# Get some input from the user
-# Convert it to a bag of words
-# Get a prediction from the model
-# Find the most probable class
-# Pick a response from that class
