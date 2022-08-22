4462cb8c817ef4ae51eb0f7059c76426bc2f3504
coppi <fietsbantje@freedom.nl>
2022-08-22 20:51:08 +0200

    add intents

--- fallback_messages.py
+++ fallback_messages.py
@@ -2,5 +2,5 @@ import random
 
 def fallback_message():
     response = ["I totally hear you, I just can't really wrap my whiskers around it yet",
-                "You talkin' to me? ... You talkin' to me? You talkin' to me?! ... If you are not talking to me, who the cavolo are you talking to?"] [random.randrange(2)]
-    return response        
+                "You talkin' to me? ... You talkin' to me?? ... You talkin' to me!?"] [random.randrange(2)]
+    return response     
