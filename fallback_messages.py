bf78358d54e424d14dae1ca26a23124462277165
coppi <fietsbantje@freedom.nl>
2022-10-13 18:44:52 +0200

    add game of life fallback message

--- fallback_messages.py
+++ fallback_messages.py
@@ -3,5 +3,8 @@ import random
 def fallback_message():
     response = ["I totally hear you, I just can't really wrap my whiskers around it yet",
                 "You talkin' to me? ... You talkin' to me?? ... You talkin' to me!?",
-                "Prrrrr...prrrrr...prrrrr...prrrrr...prrrrr..prrrprrprrrrrrprrrrr"] [random.randrange(3)]
+                "Prrrrr...prrrrr...prrrrr...prrrrr...prrrrr..prrrprrprrrrrrprrrrr",
+                "Prrrrrrrrrrrrrr...prrrrrrrrrrrrr...prrrrrrrrrrrr...prrrrrrrrrrrr",
+                "PRRRRRRRRRRRRRRRRR...PRRRRRRRRRRRRRRRR...PRRRRRRRRRRRRRRRR...PRRRPRRRPRRR"
+                "Do you want to talk some more or shall we play a game?"] [random.randrange(6)]
     return response
